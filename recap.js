14d775be6127866889259fdc4523be180e148034
Shahinur Islam <shahinur04042002@gmail.com>
2023-02-19 14:19:14 +0600

    jannat love

---
+++ recap.js
@@ -0,0 +1,15 @@
+//how to use html or css in js
+//get element or elements from dom
+//.1 get elementById
+//.2 get elementsByClassName
+//.3 QuarrySelector
+//.4 QuarrySelectorsAll
+
+//dynamically create element to the dom;document .createElement
+//.1 set inner.text   (h1,p,div,span,strong)
+//.2 set inner.value---input/textArea  type use
+
+//add evenListener/onclick() handler(click,hover,keyup,mouseEnter)
+//use function to reduce duplicate code
+//eventDelegate
+//.eventBubble
